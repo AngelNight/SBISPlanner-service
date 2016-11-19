567561df18b0c27f47b199dd1fbff0c3add9f942
tm.baeva <tm.afonina@tensor.ru>
2016-11-19 23:59:57 +0300

    six

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def request_pars(str):
             bot.send_message(221482003, "Вам нужно закрыть задачу" + "\n" + l[2])
     elif l[1] == "conference":
             bot.send_message(221482003, "Вас приглошают на совещание" + "\n" + l[2])
-    elif l[1] == "colendar":
+    elif l[1] == "calendar":
             bot.send_message(221482003, "Ваше расписание составлено" + "\n" + l[2])
 
     return l
@@ -79,3 +79,16 @@ print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
 
 
 #bot.polling()
+
+#def wsgi_app(environ, start_response):
+#    status = '200 OK'
+#    response_headers = [('Content-type', 'text/plain')]
+#    start_response(status, response_headers)
+#    response_body = 'Hello World'
+#    yield response_body.encode()
+
+#if __name__ == '__main__':
+#    from wsgiref.simple_server import make_server
+
+#    httpd = make_server('localhost', 80, wsgi_app)
+#    httpd.serve_forever()
